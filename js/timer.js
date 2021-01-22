d0be9836f1246bdb408d4da523342872593de336
Alexander <youdoit@mail.ru>
2021-01-22 15:16:43 +0600

    Fixed timer

--- js/timer.js
+++ js/timer.js
@@ -3,7 +3,6 @@ let minutes = "15";
 let seconds = "00";
 setInterval(() => {
     seconds = (+seconds - 1).toString();
-    console.log(seconds);
     if (seconds == "-1"){
         seconds = "59";
         minutes = (+minutes - 1).toString();
@@ -12,9 +11,9 @@ setInterval(() => {
         seconds = "0" + seconds;
     }
     if (+minutes < 10){
-        minutes = "0" + minutes;
+        minutes = "0" + (+minutes).toString();
     }
-    timer.innerHTML = "00" + " | " + minutes + " | " + seconds;
+    timer.innerHTML = "<strong>00</strong>" + " | " + "<strong>" +minutes + "</strong>" + " | " + "<strong>" + seconds + "</strong>";
 }, 1000);
 
  
