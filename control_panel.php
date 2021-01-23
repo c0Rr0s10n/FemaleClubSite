8d447695f5b2b7451dd06d253d4cb361af707d49
Alexander <youdoit@mail.ru>
2021-01-24 04:38:44 +0600

    Invented thing for handicaps

--- control_panel.php
+++ control_panel.php
@@ -1,56 +1,151 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V19</title>
+	<title>Table V03</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="images/icons/favicon (2).ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min (2).css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min (2).css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate (2).css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min (2).css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar (2).css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/util (2).css">
+	<link rel="stylesheet" type="text/css" href="css/main (2).css">
 <!--===============================================================================================-->
 </head>
 <body>
 	
 	<div class="limiter">
-		
+		<div class="container-table100">
+				<div class="table100 ver6 m-b-110">
+					<table data-vertable="ver6">
+						<thead>
+							<tr class="row100 head">
+								<th class="column100 column1" data-column="column1"></th>
+								<th class="column100 column2" data-column="column2">Sunday</th>
+								<th class="column100 column3" data-column="column3">Monday</th>
+								<th class="column100 column4" data-column="column4">Tuesday</th>
+								<th class="column100 column5" data-column="column5">Wednesday</th>
+								<th class="column100 column6" data-column="column6">Thursday</th>
+								<th class="column100 column7" data-column="column7">Friday</th>
+								<th class="column100 column8" data-column="column8">Saturday</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Lawrence Scott</td>
+								<td class="column100 column2" data-column="column2">8:00 AM</td>
+								<td class="column100 column3" data-column="column3">--</td>
+								<td class="column100 column4" data-column="column4">--</td>
+								<td class="column100 column5" data-column="column5">8:00 AM</td>
+								<td class="column100 column6" data-column="column6">--</td>
+								<td class="column100 column7" data-column="column7">5:00 PM</td>
+								<td class="column100 column8" data-column="column8">8:00 AM</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Jane Medina</td>
+								<td class="column100 column2" data-column="column2">--</td>
+								<td class="column100 column3" data-column="column3">5:00 PM</td>
+								<td class="column100 column4" data-column="column4">5:00 PM</td>
+								<td class="column100 column5" data-column="column5">--</td>
+								<td class="column100 column6" data-column="column6">9:00 AM</td>
+								<td class="column100 column7" data-column="column7">--</td>
+								<td class="column100 column8" data-column="column8">--</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Billy Mitchell</td>
+								<td class="column100 column2" data-column="column2">9:00 AM</td>
+								<td class="column100 column3" data-column="column3">--</td>
+								<td class="column100 column4" data-column="column4">--</td>
+								<td class="column100 column5" data-column="column5">--</td>
+								<td class="column100 column6" data-column="column6">--</td>
+								<td class="column100 column7" data-column="column7">2:00 PM</td>
+								<td class="column100 column8" data-column="column8">8:00 AM</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Beverly Reid</td>
+								<td class="column100 column2" data-column="column2">--</td>
+								<td class="column100 column3" data-column="column3">5:00 PM</td>
+								<td class="column100 column4" data-column="column4">5:00 PM</td>
+								<td class="column100 column5" data-column="column5">--</td>
+								<td class="column100 column6" data-column="column6">9:00 AM</td>
+								<td class="column100 column7" data-column="column7">--</td>
+								<td class="column100 column8" data-column="column8">--</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Tiffany Wade</td>
+								<td class="column100 column2" data-column="column2">8:00 AM</td>
+								<td class="column100 column3" data-column="column3">--</td>
+								<td class="column100 column4" data-column="column4">--</td>
+								<td class="column100 column5" data-column="column5">8:00 AM</td>
+								<td class="column100 column6" data-column="column6">--</td>
+								<td class="column100 column7" data-column="column7">5:00 PM</td>
+								<td class="column100 column8" data-column="column8">8:00 AM</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Sean Adams</td>
+								<td class="column100 column2" data-column="column2">--</td>
+								<td class="column100 column3" data-column="column3">5:00 PM</td>
+								<td class="column100 column4" data-column="column4">5:00 PM</td>
+								<td class="column100 column5" data-column="column5">--</td>
+								<td class="column100 column6" data-column="column6">9:00 AM</td>
+								<td class="column100 column7" data-column="column7">--</td>
+								<td class="column100 column8" data-column="column8">--</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Rachel Simpson</td>
+								<td class="column100 column2" data-column="column2">9:00 AM</td>
+								<td class="column100 column3" data-column="column3">--</td>
+								<td class="column100 column4" data-column="column4">--</td>
+								<td class="column100 column5" data-column="column5">--</td>
+								<td class="column100 column6" data-column="column6">--</td>
+								<td class="column100 column7" data-column="column7">2:00 PM</td>
+								<td class="column100 column8" data-column="column8">8:00 AM</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1">Mark Salazar</td>
+								<td class="column100 column2" data-column="column2">8:00 AM</td>
+								<td class="column100 column3" data-column="column3">--</td>
+								<td class="column100 column4" data-column="column4">--</td>
+								<td class="column100 column5" data-column="column5">8:00 AM</td>
+								<td class="column100 column6" data-column="column6">--</td>
+								<td class="column100 column7" data-column="column7">5:00 PM</td>
+								<td class="column100 column8" data-column="column8">8:00 AM</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
 	</div>
-    
+
 
 	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min (2).js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/bootstrap/js/popper (2).js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min (2).js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendor/select2/select2.min (2).js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
+	<script src="js/main (2).js"></script>
 
 </body>
 </html>
