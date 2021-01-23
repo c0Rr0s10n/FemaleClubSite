b9e818ef5c717c518a5a846a81349682f675c42a
Alexander <youdoit@mail.ru>
2021-01-24 04:42:31 +0600

    Prepared table

--- control_panel.php
+++ control_panel.php
@@ -29,14 +29,10 @@
 					<table data-vertable="ver6">
 						<thead>
 							<tr class="row100 head">
-								<th class="column100 column1" data-column="column1"></th>
-								<th class="column100 column2" data-column="column2">Sunday</th>
-								<th class="column100 column3" data-column="column3">Monday</th>
-								<th class="column100 column4" data-column="column4">Tuesday</th>
-								<th class="column100 column5" data-column="column5">Wednesday</th>
-								<th class="column100 column6" data-column="column6">Thursday</th>
-								<th class="column100 column7" data-column="column7">Friday</th>
-								<th class="column100 column8" data-column="column8">Saturday</th>
+								<th class="column100 column1" data-column="column1">Id</th>
+								<th class="column100 column2" data-column="column2">Blogger</th>
+								<th class="column100 column3" data-column="column3">Promo code</th>
+								<th class="column100 column4" data-column="column4">Amount of passed</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -45,10 +41,6 @@
 								<td class="column100 column2" data-column="column2">8:00 AM</td>
 								<td class="column100 column3" data-column="column3">--</td>
 								<td class="column100 column4" data-column="column4">--</td>
-								<td class="column100 column5" data-column="column5">8:00 AM</td>
-								<td class="column100 column6" data-column="column6">--</td>
-								<td class="column100 column7" data-column="column7">5:00 PM</td>
-								<td class="column100 column8" data-column="column8">8:00 AM</td>
 							</tr>
 
 							<tr class="row100">
@@ -56,10 +48,6 @@
 								<td class="column100 column2" data-column="column2">--</td>
 								<td class="column100 column3" data-column="column3">5:00 PM</td>
 								<td class="column100 column4" data-column="column4">5:00 PM</td>
-								<td class="column100 column5" data-column="column5">--</td>
-								<td class="column100 column6" data-column="column6">9:00 AM</td>
-								<td class="column100 column7" data-column="column7">--</td>
-								<td class="column100 column8" data-column="column8">--</td>
 							</tr>
 
 							<tr class="row100">
@@ -67,21 +55,12 @@
 								<td class="column100 column2" data-column="column2">9:00 AM</td>
 								<td class="column100 column3" data-column="column3">--</td>
 								<td class="column100 column4" data-column="column4">--</td>
-								<td class="column100 column5" data-column="column5">--</td>
-								<td class="column100 column6" data-column="column6">--</td>
-								<td class="column100 column7" data-column="column7">2:00 PM</td>
-								<td class="column100 column8" data-column="column8">8:00 AM</td>
-							</tr>
 
 							<tr class="row100">
 								<td class="column100 column1" data-column="column1">Beverly Reid</td>
 								<td class="column100 column2" data-column="column2">--</td>
 								<td class="column100 column3" data-column="column3">5:00 PM</td>
 								<td class="column100 column4" data-column="column4">5:00 PM</td>
-								<td class="column100 column5" data-column="column5">--</td>
-								<td class="column100 column6" data-column="column6">9:00 AM</td>
-								<td class="column100 column7" data-column="column7">--</td>
-								<td class="column100 column8" data-column="column8">--</td>
 							</tr>
 
 							<tr class="row100">
@@ -89,10 +68,6 @@
 								<td class="column100 column2" data-column="column2">8:00 AM</td>
 								<td class="column100 column3" data-column="column3">--</td>
 								<td class="column100 column4" data-column="column4">--</td>
-								<td class="column100 column5" data-column="column5">8:00 AM</td>
-								<td class="column100 column6" data-column="column6">--</td>
-								<td class="column100 column7" data-column="column7">5:00 PM</td>
-								<td class="column100 column8" data-column="column8">8:00 AM</td>
 							</tr>
 
 							<tr class="row100">
@@ -100,10 +75,6 @@
 								<td class="column100 column2" data-column="column2">--</td>
 								<td class="column100 column3" data-column="column3">5:00 PM</td>
 								<td class="column100 column4" data-column="column4">5:00 PM</td>
-								<td class="column100 column5" data-column="column5">--</td>
-								<td class="column100 column6" data-column="column6">9:00 AM</td>
-								<td class="column100 column7" data-column="column7">--</td>
-								<td class="column100 column8" data-column="column8">--</td>
 							</tr>
 
 							<tr class="row100">
@@ -111,10 +82,6 @@
 								<td class="column100 column2" data-column="column2">9:00 AM</td>
 								<td class="column100 column3" data-column="column3">--</td>
 								<td class="column100 column4" data-column="column4">--</td>
-								<td class="column100 column5" data-column="column5">--</td>
-								<td class="column100 column6" data-column="column6">--</td>
-								<td class="column100 column7" data-column="column7">2:00 PM</td>
-								<td class="column100 column8" data-column="column8">8:00 AM</td>
 							</tr>
 
 							<tr class="row100">
@@ -122,11 +89,6 @@
 								<td class="column100 column2" data-column="column2">8:00 AM</td>
 								<td class="column100 column3" data-column="column3">--</td>
 								<td class="column100 column4" data-column="column4">--</td>
-								<td class="column100 column5" data-column="column5">8:00 AM</td>
-								<td class="column100 column6" data-column="column6">--</td>
-								<td class="column100 column7" data-column="column7">5:00 PM</td>
-								<td class="column100 column8" data-column="column8">8:00 AM</td>
-							</tr>
 						</tbody>
 					</table>
 				</div>
