a3ba4cec1cf79a7ba2ff92065a8c19b1a2d1736f
Alexander <youdoit@mail.ru>
2021-01-25 03:54:42 +0600

    Trying figuring some problems out

--- control_panel.php
+++ control_panel.php
@@ -22,22 +22,10 @@
 <!--===============================================================================================-->
 </head>
 <body>
-<?php
-         $dbhost = 'localhost:3036';
-         $dbuser = 'guest1';
-         $dbpass = 'guest1a';
-		 $dbname = 'onemusec_data';
-         $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-		if ($conn->connect_error) {
-		  die("Database connection failed: " . $conn->connect_error);
-		}
-         echo 'Connected successfully';
-         mysql_close($conn);
-      ?>
 	<div class="limiter">
 		<div class="container-table100">
 				<div class="table100 ver6 m-b-110">
-					<!-- <table data-vertable="ver6">
+					<table data-vertable="ver6">
 						<thead>
 							<tr class="row100 head">
 								<th class="column100 column1" data-column="column1">Blogger</th>
@@ -45,57 +33,30 @@
 								<th class="column100 column3" data-column="column3">Amount of passed</th>
 							</tr>
 						</thead>
-						<tbody> -->
-						
+						<tbody>
+							<?php
+							 $dbhost = 'https://srv-plesk48.ps.kz:8443';
+							 $dbuser = 'onemusec_admin';
+							 $dbpass = 'V3K3~5qtc*4n';
+							 $dbname = 'onemusec_data';
+							 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+							if ($conn->connect_error) {
+							  die("Database connection failed: " . $conn->connect_error);
+							}
+							 $query = mysqli_query($dbconnect, "SELECT * FROM promos")
+   								or die (mysqli_error($dbconnect));
+							while ($row = mysqli_fetch_array($query)) {
+  								echo 
+								'<tr class="row100">
+								<td class="column100 column1" data-column="column1">'.$row['blogger'].'</td>
+								<td class="column100 column2" data-column="column2">'.$row['promo']'</td>
+								<td class="column100 column3" data-column="column3">'.$row['amount'].'</td>
+							</tr>';
+							}
+						  ?>
 
-							<!-- <tr class="row100">
-								<td class="column100 column1" data-column="column1">Lawrence Scott</td>
-								<td class="column100 column2" data-column="column2">8:00 AM</td>
-								<td class="column100 column3" data-column="column3">--</td>
-							</tr>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Jane Medina</td>
-								<td class="column100 column2" data-column="column2">--</td>
-								<td class="column100 column3" data-column="column3">5:00 PM</td>
-							</tr>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Billy Mitchell</td>
-								<td class="column100 column2" data-column="column2">9:00 AM</td>
-								<td class="column100 column3" data-column="column3">--</td>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Beverly Reid</td>
-								<td class="column100 column2" data-column="column2">--</td>
-								<td class="column100 column3" data-column="column3">5:00 PM</td>
-							</tr>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Tiffany Wade</td>
-								<td class="column100 column2" data-column="column2">8:00 AM</td>
-								<td class="column100 column3" data-column="column3">--</td>
-							</tr>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Sean Adams</td>
-								<td class="column100 column2" data-column="column2">--</td>
-								<td class="column100 column3" data-column="column3">5:00 PM</td>
-							</tr>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Rachel Simpson</td>
-								<td class="column100 column2" data-column="column2">9:00 AM</td>
-								<td class="column100 column3" data-column="column3">--</td>
-							</tr>
-
-							<tr class="row100">
-								<td class="column100 column1" data-column="column1">Mark Salazar</td>
-								<td class="column100 column2" data-column="column2">8:00 AM</td>
-								<td class="column100 column3" data-column="column3">--</td> -->
-							<!-- </tr> -->
-						<!-- </tbody>
-					</table> -->
+						</tbody>
+					</table>
 					<!-- <form action="" method="post">
 						<input type="text" name="Blogger" id="" placeholder="Name of blogger">
 						<input type="text" name="Blogger" id="" placeholder="Promo">
@@ -103,9 +64,6 @@
 				</div>
 			</div>
 		</div>
-
-
-	
 
 <!--===============================================================================================-->	
 	<script src="vendor/jquery/jquery-3.2.1.min (2).js"></script>
