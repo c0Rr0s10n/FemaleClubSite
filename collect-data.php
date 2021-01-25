ec3bbcd716edb0922e4194c92ccf44f9c1080e3e
Alexander <youdoit@mail.ru>
2021-01-26 01:47:37 +0600

    Almost there

--- collect-data.php
+++ collect-data.php
@@ -5,8 +5,12 @@ $dbpass = 'V3K3~5qtc*4n';
 $dbname = 'onemusec_data';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
+    die();
 }
-$query = mysqli_query($conn, "INSERT INTO bills ('number', 'session') VALUES ('".$_POST['id'].','324'")
-                    or die (mysqli_error($conn));
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+file_put_contents('request.txt', $data['id']);
+$b_id = $data['id'];
+$status = $data['status'];
+mysqli_query($conn, "INSERT INTO bills (number, status) VALUES ('{$b_id}', {status})";
 ?>
