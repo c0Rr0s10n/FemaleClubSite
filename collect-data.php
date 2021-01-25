93a4257d74e63d56b77ff455a6782eb48af2ac12
Alexander <youdoit@mail.ru>
2021-01-26 02:02:10 +0600

    I am giving up

--- collect-data.php
+++ collect-data.php
@@ -4,13 +4,19 @@ $dbuser = 'onemusec_admin';
 $dbpass = 'V3K3~5qtc*4n';
 $dbname = 'onemusec_data';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-if (mysqli_connect_errno()) {
-    die();
+if ($mysqli -> connect_errno) {
+  file_put_contents('request.txt', "Failed to connect to MySQL: " . $mysqli -> connect_error);
+  exit();
 }
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 file_put_contents('request.txt', $data['id']);
 $b_id = $data['id'];
 $status = $data['status'];
-mysqli_query($conn, "INSERT INTO bills (number, status) VALUES ('{$b_id}', {status})";
+$sql = "INSERT INTO bills (number, status) VALUES ('".$b_id."', '".$status."');";
+if (mysqli_query($conn, $sql)) {
+ echo 'ok';
+} else {
+ file_put_contents('request.txt', "Error: " . $sql . "<br>" . mysqli_error($conn));
+}
 ?>
