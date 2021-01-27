f97ace905455547520b9e1dbeed5e984950e9ee2
Alexander <youdoit@mail.ru>
2021-01-28 03:21:21 +0600

    Finished additional glowing around input

--- js/test.js
+++ js/test.js
@@ -1,12 +1,15 @@
 let promo_input = document.querySelector('.promo-code');
 let codes_string = document.querySelector('.codes').value;
 let codes = codes_string.split(" ");
+let textt = document.querySelector('.clarify');
 codes = codes.slice(0, codes.length-1);
 promo_input.oninput = () => {
     if (codes.includes(promo_input.value)){
         promo_input.style.border = "1px solid green";
+		textt.innerHTML = "Введён верный промокод!<br> <strong>К вашей оплате будет начислена скидка</strong>";
     }
     else {
         promo_input.style.border = "1px solid red";
+		textt.innerHTML = "Промокод не верный.";
     }
 }
