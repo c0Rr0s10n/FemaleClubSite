fd5edf0cc93cc2569331b37da61315c61de9165a
Alibek <venommega2001@gmail.com>
2021-01-29 02:56:02 +0600

    Pohiu

---
+++ accessed-bill.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style1.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div id="success" class="text-center">
+        <div class="container my-auto">
+            <div class="row my-auto">
+                <div class="col align-self-center">
+                    <img src="/img/success.png" alt="">
+                    <span class="">Оплата прошла успешно</span>
+                    <p>Ваша ссылка на группу на <a href="">WhatsApp</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script type="text/javascript" src="js/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ declined-bill.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style2.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div id="declined" class="text-center">
+        <div class="container my-auto">
+            <div class="row my-auto">
+                <div class="col align-self-center">
+                    <img src="img/declined.png" alt="">
+                    <span class="">Упс... что-то пошло не так</span>
+                    <p>Повторите платеж снова</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script type="text/javascript" src="js/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
